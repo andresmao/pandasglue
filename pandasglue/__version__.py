6362e83173a98f1d9e1fba6a1abe631439b16840
Igor Tavares <igorborgest@gmail.com>
2019-02-07 21:50:29 -0200

    adding some forgetten files

--- pandasglue/__version__.py
+++ pandasglue/__version__.py
@@ -1,4 +1,4 @@
 __title__ = "pandasglue"
-__description__ = "Pandas and AWS Glue integration"
+__description__ = "Productivity for your Data Lake"
 __version__ = "0.0.0"
-__license__ = ""
+__license__ = "Apache License 2.0"
